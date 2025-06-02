414236e75f42dc07b9c825737a4908bb572fd5e9
elvismartis <elvis.afmartis@gmail.com>
2025-06-02 14:46:17 +0200

    added necessary details for prepare_for_piper.py

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PepViScreen
+:# PepViScreen
  ![image](image.svg)
 
 PEPtide VIrtual Screening using PIPER and sblu. You can now run Cluspro Workflow locally. 
@@ -9,17 +9,21 @@ PEPtide VIrtual Screening using PIPER and sblu. You can now run Cluspro Workflow
 ```
 git clone https://github.com/elvismartis/PepViScreen.git
 ```
+## Steps for installation and setup
+```
+python3 -m  -m venv </path>/piper
+source </path>/piper/bin/activate
+python3 -m pip install -r requirements.txt 
+```
 
 ## Requirements:
-1. sblu: Python library and associated scripts for munging data files from ClusPro/FTMap/etc.. For more details, visit [sblu - PyPI](https://pypi.org/project/sblu/).
 
-You must install it as follows:
+1. **sblu**: Python library and associated scripts for munging data files from ClusPro/FTMap/etc.. For more details, visit [sblu - PyPI](https://pypi.org/project/sblu/).
 
-```
-[sudo] pip install sblu
-```
- 2. Piper: FFT-based docking software used in ClusPro. It is available as a 64-bit executable for use on Linux systems. Obtain PIPER from the web server [Cluspro](https://cluspro.bu.edu/downloads.php). You need to register and download the Piper binary.
+ 2. **Piper**: FFT-based docking software used in ClusPro. It is available as a 64-bit executable for use on Linux systems. Obtain PIPER from the web server [Cluspro](https://cluspro.bu.edu/downloads.php). You need to register and download the Piper binary.
 **PIPER is open-source for academic users ONLY**
+
+3. **prepare_for_piper.py**: This python script prepares the PDB file compatible with PIPER. This script is modification of the prepare.py that is bundled with piper_package. The original bundled python script and the dependencies on pdb2pqr-1.9.0 work only on python2.7. prepare_for_piper.py is a workaround for using latest pdq2pqr and python3. 
 
 ## Usage
 To perform **protein-protein** or **protein-peptide** docking with PIPER and extract docked conformation as a PDB file. 
@@ -30,10 +34,6 @@ sh run_multidocking.sh
  To prepare the receptor protein, execute the following command before running "run_multidocking.sh". This needs to be done once for each Receptor structure. The output will be "**XYZ_pnon.pdb**". Place this file in a directory; the PATH to this directory needs to be added in the main BASH script at "REC="
 
  ```
- $PIPER_HOME/protein_prep/prepare.py XYZ.pdb
+ $PIPER_HOME/prepare_for_piper.py XYZ.pdb
 ```
-## Known Issue:
-- **prepare.py** needs python2.7.
-- Workaround:
-              - UCSFchimera1.17 users can source python2.7 from chimera installation.
-              - Others can try to build a virtualenv for Python 2.7.
+## Known Issues 
---
+++ prepare_for_piper.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python3
+import subprocess
+import argparse
+import os.path
+import sys
+import os
+
+def add_chain(file, out_file):
+    """
+    If chain identifier is absent in a given PDB
+    add_chain with add chain ID as "A"
+    """
+    with open(out_file, 'w') as out_f, open(file) as f:
+        for line in f:
+            if line.startswith('ATOM  ') or line.startswith('HETATM'):
+                if line[21] == " ":
+                    line = line[:21] + "A" + line[22:]
+                out_f.write(line)
+
+def extract_chains(file, chains, out_file):
+    """
+    If chain identifiers are present in the given PDB
+    extract_chain will extract and save in "chains" variable
+    to use later.
+    """
+    with open(out_file, 'w') as out_f, open(file) as f:
+        for line in f:
+            if line.startswith('ATOM  ') or line.startswith('HETATM'):
+                if line[21] in chains:
+                    out_f.write(line)
+
+def filter_missing_backbone(pdb_in, pdb_out):
+    """
+    When there are gaps in the tertiary,
+    filter_missing_backbone will extract all good atoms 
+    from continous chain
+    """
+    f = open(pdb_in, 'r')
+    lines = f.readlines()
+    f.close()
+
+    atom_i = 0
+    atoms = []
+    for line in lines:
+        if line.startswith('ATOM  '):
+            atom_i +=  1
+            chain_id =  line[21]
+            residue_id =  line[22:27].strip()
+            residue_name =  line[17:21].strip()
+            atom_name = line[12:16].strip()
+            atoms.append({'atom_i': atom_i, 'chain_id': chain_id, 'residue_id': residue_id,
+							'residue_name': residue_name, 'atom_name': atom_name, 'line': line })
+
+
+    pdb_residue_list = set()
+    for atom in atoms:
+        pdb_residue_list.add( (atom['chain_id'], atom['residue_name'], atom['residue_id']) )
+
+    pdb_residues = []
+    for residue in pdb_residue_list:
+        pdb_residues.append( list(filter(lambda x: x['chain_id'] == residue[0] and
+									x['residue_id'] == residue[2] and x['residue_name'] == residue[1], atoms ) ))
+
+
+    good_atoms = []
+    for residue in pdb_residues:
+        has_CA = False
+        has_C = False
+        has_N = False
+        for atom in residue:
+            if atom['atom_name'] == 'N':
+                has_N = True
+            if atom['atom_name'] == 'C':
+                has_C = True
+            if atom['atom_name'] == 'CA':
+                has_CA = True
+        if has_N and has_C and has_CA:
+            for atom in residue:
+                good_atoms.append(atom)
+
+    good_atoms.sort(key=lambda x: x['atom_i'])
+
+    f = open(pdb_out, 'w')
+    for atom in good_atoms:
+        f.write(atom['line'])
+    f.close()
+
+
+def pdb_norm(in_pdb, out_pdb):
+    """
+    Atoms from each amino acid to retain in the output *_pnon.pdb
+    Add modified amino acids here to retain them.
+    Residues not in this list will be stripped.
+    """
+    resis = dict(ALA=['N', 'H', 'CA', 'CB', 'C', 'O'],
+                 ARG=['N', 'H', 'CA', 'CB', 'CG', 'CD', 'NE', 'HE', 'CZ', 'NH1', 'HH11', 'HH12', 'NH2', 'HH21', 'HH22', 'C', 'O'],
+                 ARN=['N', 'H', 'CA', 'CB', 'CG', 'CD', 'NE', 'HE', 'CZ', 'NH1', 'HH11', 'HH12', 'NH2', 'HH21', 'C', 'O'],
+                 ASN=['N', 'H', 'CA', 'CB', 'CG', 'OD1', 'ND2', 'HD21', 'HD22', 'C', 'O'],
+                 ASP=['N', 'H', 'CA', 'CB', 'CG', 'OD1', 'OD2', 'C', 'O'],
+                 ASH=['N', 'H', 'CA', 'CB', 'CG', 'OD1', 'OD2', 'HD2', 'C', 'O'],
+                 CYS=['N', 'H', 'CA', 'CB', 'SG', 'C', 'O'],
+                 CYX=['N', 'H', 'CA', 'CB', 'SG', 'C', 'O'],
+                 GLN=['N', 'H', 'CA', 'CB', 'CG', 'CD', 'OE1', 'NE2', 'HE21', 'HE22', 'C', 'O'],
+                 GLU=['N', 'H', 'CA', 'CB', 'CG', 'CD', 'OE1', 'OE2', 'C', 'O'],
+                 GLH=['N', 'H', 'CA', 'CB', 'CG', 'CD', 'OE1', 'OE2', 'HE2', 'C', 'O'],
+                 GLY=['N', 'H', 'CA', 'C', 'O'],
+                 HIS=['N', 'H', 'CA', 'CB', 'CG', 'ND1', 'HD1', 'CD2', 'NE2', 'CE1', 'C', 'O'],
+                 HID=['N', 'H', 'CA', 'CB', 'CG', 'ND1', 'HD1', 'CD2', 'NE2', 'CE1', 'C', 'O'],
+                 HIP=['N', 'H', 'CA', 'CB', 'CG', 'CD2', 'ND1', 'HD1', 'CE1', 'NE2', 'HE2', 'C', 'O'],
+                 HIE=['N', 'H', 'CA', 'CB', 'CG', 'ND1', 'CE1', 'CD2', 'NE2', 'HE2', 'C', 'O'],
+                 ILE=['N', 'H', 'CA', 'CB', 'CG2', 'CG1', 'CD1', 'C', 'O'],
+                 LEU=['N', 'H', 'CA', 'CB', 'CG', 'CD1', 'CD2', 'C', 'O'],
+                 LYS=['N', 'H', 'CA', 'CB', 'CG', 'CD', 'CE', 'NZ', 'HZ1', 'HZ2', 'HZ3', 'C', 'O'],
+                 LYN=['N', 'H', 'CA', 'CB', 'CG', 'CD', 'CE', 'NZ', 'HZ1', 'HZ2', 'C', 'O'],
+                 MET=['N', 'H', 'CA', 'CB', 'CG', 'SD', 'CE', 'C', 'O'],
+                 MSE=['N', 'H', 'CA', 'CB', 'CG', 'SE', 'CE', 'C', 'O'],
+                 PHE=['N', 'H', 'CA', 'CB', 'CG', 'CD1', 'CD2', 'CE1', 'CE2', 'CZ', 'C', 'O'],
+                 PRO=['N', 'CD', 'CA', 'CB', 'CG', 'C', 'O'],
+                 SER=['N', 'H', 'CA', 'CB', 'OG', 'HG', 'C', 'O'],
+                 THR=['N', 'H', 'CA', 'CB', 'OG1', 'HG1', 'CG2', 'C', 'O'],
+                 TRP=['N', 'H', 'CA', 'CB', 'CG', 'CD2', 'CE2', 'CE3', 'CD1', 'NE1', 'HE1', 'CZ2', 'CZ3', 'CH2', 'C', 'O'],
+                 TYR=['N', 'H', 'CA', 'CB', 'CG', 'CD1', 'CD2', 'CE1', 'CE2', 'CZ', 'OH', 'HH', 'C', 'O'],
+                 VAL=['N', 'H', 'CA', 'CB', 'CG1', 'CG2', 'C', 'O']
+    )
+    atoms = []
+    with open(in_pdb) as pdb:
+        for line in pdb:
+            if line.startswith('ATOM  '):
+                chain_id =  line[21]
+                residue_num =  int(line[22:26])
+                residue_alt =  line[26]
+                residue_id = str(residue_num) + residue_alt
+                residue_name =  line[17:21].strip()
+                atom_name = line[12:16].strip()
+                atoms.append({'chain_id': chain_id, 'residue_num': residue_num, 'residue_alt': residue_alt, 'residue_id': residue_id, 'residue_name': residue_name, 'atom_name': atom_name, 'line': line })
+
+    pdb_residue_list = set()
+    for atom in atoms:
+        pdb_residue_list.add( (atom['chain_id'], atom['residue_num'], atom['residue_alt']) )
+
+    pdb_residue_list = sorted(pdb_residue_list, key=lambda x: (ord(x[0])*100000*100) + x[1]*100 + ord(x[2]) )
+
+    pdb_residues = []
+    for residue in pdb_residue_list:
+        residue_id = str(residue[1]) + residue[2]
+        pdb_residues.append( list(filter(lambda x: x['chain_id'] == residue[0] and x['residue_id'] == residue_id, atoms ) ))
+
+    atom_i = 1
+    with open(out_pdb, 'w') as out_f:
+        for residue in pdb_residues:
+            residue_name = residue[0]['residue_name']
+            if residue_name not in resis:
+                print ("Residue %s not supported, stripped for mapping" % residue_name, file=sys.stderr)
+                continue
+            residue_atoms = resis[residue_name]
+            for resi_atom in residue_atoms:
+                for pdb_atom in residue:
+                    if pdb_atom['atom_name'] == resi_atom:
+                        atom = "%5g" %atom_i
+                        out_f.write(pdb_atom['line'][0:6] + atom + pdb_atom['line'][11:])
+                        atom_i += 1
+                        break
+
+
+
+def pdbpqr(base_dir, pdb, chains):
+    """
+    Runs pdb2pqr.
+    incase titrable residue, define appropriate pH.
+    Use pbd2pqr flag "--titrate_only A:10,A:11 
+    --titration-state-method propka --with-pH pH"
+    """
+    pdb_prefix = pdb[:-4]
+
+    genout       = pdb_prefix+"_pgen.pdb";
+    pqr          = pdb_prefix+"_pmin.pqr";
+    nonminout    = pdb_prefix+"_pnon.pdb";
+    minlog       = pdb_prefix+"_pmin.log";
+
+
+    chains = [x.upper() for x in chains]
+
+    if len(chains) == 0:
+        add_chain(pdb, genout)
+    else:
+        extract_chains(pdb, chains, genout)
+
+    filter_missing_backbone(genout, genout)
+    with open('error_log', 'a') as err_out, open('output_log', 'a') as out_f:
+        subprocess.call(['pdb2pqr', '--nodebump', '--noopt', '--ff=AMBER',
+							'--ffout=AMBER', genout, pqr],
+							stdout=out_f, stderr=err_out)
+    pdb_norm(genout, nonminout)
+
+    os.unlink(genout)
+    os.unlink(pqr)
+    os.unlink(minlog)
+    os.unlink('error_log')
+    os.unlink('output_log')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('pdb_file')
+    parser.add_argument('chains', nargs='*')
+    base_dir=os.path.dirname(os.path.abspath(sys.argv[0]))
+    args=parser.parse_args()
+    pdbpqr(base_dir, args.pdb_file, args.chains)
